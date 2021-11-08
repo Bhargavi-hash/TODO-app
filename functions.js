0df1c4058bcc456647248fdcf71c2d03721afd31
Bhargavi <82858718+Bhargavi-hash@users.noreply.github.com>
2021-11-08 12:13:00 +0530

    Update functions.js

--- functions.js
+++ functions.js
@@ -12,6 +12,12 @@ window.addEventListener('load', () => {
 
 
         const task = input.value;
+        
+        if (!task)
+        {
+            alert("Please fill out the task !");
+            return;
+        }
 
         const task_el = document.createElement('div');
         task_el.classList.add('task');
